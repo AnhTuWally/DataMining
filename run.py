93f2a1c163e93e41b5051b70ff213c2c19f97816
wallymediacenter <mcassist@M1611668.local>
2016-10-11 17:12:58 -0400

    works with the text file

--- run.py
+++ run.py
@@ -20,124 +20,125 @@ f.write('\t'+'number_shares'+ '\t' + 'value'+ '\n')
 
 #Initial editing
 for i in range(len(html)):
-    if '<TR' in html[i]:
-        #flag to signal the end of tr
-        notEndTR = True
-        count = 0
-        row = ''
-        while notEndTR:
-            try:
+    try:
+        if '<TR' in html[i]:
+            #flag to signal the end of tr
+            notEndTR = True
+            count = 0
+            row = ''
+            while notEndTR:
                 line = html[i]
-            except IndexError:
-                print i
-            #check if the tag </TR> is in the line
-            notEndTR = '</TR>' not in line #termination condition
-            #Processing the line
-            for j in range(len(line)):
-                data = '' #empty string that store the data
-                #detect the data
-                if line[j] == '>':
-                    #avoid writing >
-                    if j < len(line)-1: j+=1
+                #check if the tag </TR> is in the line
+                notEndTR = '</TR>' not in line #termination condition
+                #Processing the line
+                for j in range(len(line)):
+                    data = '' #empty string that store the data
+                    #detect the data
+                    if line[j] == '>':
+                        #avoid writing >
+                        if j < len(line)-1: j+=1
 
-                    while line[j] != '<' and j < len(line)-1:
-                        #f.write(line[j])
+                        while line[j] != '<' and j < len(line)-1:
+                            #f.write(line[j])
 
-                        #delete the dorlar sign
-                        if line[j] == '$':
-                            pass
-                        else:
-                            data+=line[j]
-                        #increment
-                        j+=1
+                            #delete the dorlar sign
+                            if line[j] == '$':
+                                pass
+                            else:
+                                data+=line[j]
+                            #increment
+                            j+=1
 
-                    #replacing item with [space]
-                    data = '' if data == '&nbsp;' else data
-                    #seperator between data
-                    if '&nbsp;' not in line and '$' not in line:
-                        if data !='':
-                            #print data
-                            data = '\t' + data
-                    row += data
-                    #f.write(data)
-                #increment to next td
-                j+=1
+                        #replacing item with [space]
+                        data = '' if data == '&nbsp;' else data
+                        #seperator between data
+                        if '&nbsp;' not in line and '$' not in line:
+                            if data !='':
+                                #print data
+                                data = '\t' + data
+                        row += data
+                        #f.write(data)
+                    #increment to next td
+                    j+=1
 
-            #found an indentation mark
-            if 'TEXT-INDENT:' in html[i]:
-                startIndent = html[i].find('TEXT-INDENT:')+13
-                endIndent = html[i].find('pt')
-                #update Indentation of the current indent
-                rowIndent = float(html[i][startIndent:endIndent])
-                #print rowIndent
-                try:
-                    #find the next indentation
-                    nx = i+1
-                    while 'TEXT-INDENT:' not in html[nx]:
-                        nx+=1
-
-                    #print html[nx]
-                    startIndent = html[nx].find('TEXT-INDENT:')+13
-                    endIndent = html[nx].find('pt')
+                #found an indentation mark
+                if 'TEXT-INDENT:' in html[i]:
+                    startIndent = html[i].find('TEXT-INDENT:')+13
+                    endIndent = html[i].find('pt')
                     #update Indentation of the current indent
-                    nextIndent = float(html[nx][startIndent:endIndent])
-                except IndexError:
-                    nextIndent = rowIndent
+                    rowIndent = float(html[i][startIndent:endIndent])
+                    #print rowIndent
+                    try:
+                        #find the next indentation
+                        nx = i+1
+                        while 'TEXT-INDENT:' not in html[nx]:
+                            nx+=1
 
-            #increment to next tr
-            i+=1
+                        #print html[nx]
+                        startIndent = html[nx].find('TEXT-INDENT:')+13
+                        endIndent = html[nx].find('pt')
+                        #update Indentation of the current indent
+                        nextIndent = float(html[nx][startIndent:endIndent])
+                    except IndexError:
+                        nextIndent = rowIndent
 
-        #TR ended
-        if row and row[0]=='\t':
-            row = row[1:]
+                #increment to next tr
+                i+=1
 
-        rowArray = row.split('\t')
-        if rowArray[0]:
-            validData = True
-            if len(rowArray) >= 2:
-                data1 = rowArray.pop()
-                data2 = rowArray.pop()
-                try:
-                    int(data1[0])
-                    int(data2[0])
-                except ValueError:
-                    validData = False
-                data3 = '\t' + data2 + '\t' + data1
-                data4 = ' '.join(rowArray)
-                row = data4 + data3
-                rowArray = row.split('\t')
-            if validData:
-                #If there is a row less than 2
-                if len(rowArray) < 2:
-                    #indentation fix
-                    #print rowIndent, ' - ', nextIndent
-                    if rowIndent < nextIndent:
-                        fix = True
-                        firstRow = True
-                    else:
-                        #pass if the row indentation is too big, signify the change of page
-                        if abs(rowIndent - nextIndent) > 100:
-                            pass
+            #TR ended
+            if row and row[0]=='\t':
+                row = row[1:]
+
+            rowArray = row.split('\t')
+            if rowArray[0]:
+                validData = True
+                if len(rowArray) >= 2:
+                    data1 = rowArray.pop()
+                    data2 = rowArray.pop()
+                    try:
+                        int(data1[0])
+                        int(data2[0])
+                    except ValueError:
+                        validData = False
+                    data3 = '\t' + data2 + '\t' + data1
+                    data4 = ' '.join(rowArray)
+                    row = data4 + data3
+                    rowArray = row.split('\t')
+                if validData:
+                    #If there is a row less than 2
+                    if len(rowArray) < 2:
+                        #indentation fix
+                        #print rowIndent, ' - ', nextIndent
+                        if rowIndent < nextIndent:
+                            fix = True
+                            firstRow = True
                         else:
-                            row += '\n'
+                            #pass if the row indentation is too big, signify the change of page
+                            if abs(rowIndent - nextIndent) > 100:
+                                pass
+                            else:
+                                row += '\n'
+                                fix = False
+                                firstRow = False
+                        #the change is too big, this is a change in page
+                        if abs(rowIndent - nextIndent) < 100:
+                            prefix = row + ' '
+                    #If the row array is not less than 2, but it need to be fixed
+                    elif fix:
+                        row = prefix + row + '\n'
+                        if 1 < abs(rowIndent - nextIndent) < 100:
                             fix = False
-                            firstRow = False
-                    #the change is too big, this is a change in page
-                    if abs(rowIndent - nextIndent) < 100:
-                        prefix = row + ' '
-                #If the row array is not less than 2, but it need to be fixed
-                elif fix:
-                    row = prefix + row + '\n'
-                    if 1 < abs(rowIndent - nextIndent) < 100:
-                        fix = False
-                else:
-                    row += '\n'
+                    else:
+                        row += '\n'
 
-                #dont'r write the first row of indentation
-                if firstRow:
-                    firstRow = False
-                    pass
-                else:
-                    if len(rowArray) > 2:
-                        f.write(row)
+                    #dont'r write the first row of indentation
+                    if firstRow:
+                        firstRow = False
+                        pass
+                    else:
+                        if len(rowArray) > 2:
+                            f.write(row)
+    except IndexError:
+        #handeling the mess at the end of text file
+        break
 f.close()
