817864b542b7f677ba23aa5b25d86ee71a4fe7e9
wallymediacenter <mcassist@M1611668.local>
2016-10-11 16:08:08 -0400

    Is working properly, need a few bug fix

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import urllib2
-import re
 
 url = 'https://www.sec.gov/Archives/edgar/data/757010/000075701012000025/n-qftftpe053112.htm'
 
@@ -12,6 +11,9 @@ f = open('test.txt', 'w')
 a = urllib2.urlopen(url)
 html = a.readlines()
 
+#initialize rowIndent
+rowIndent = 0
+nextIndent = 0
 
 #Initial editing
 for i in range(len(html)):
@@ -54,8 +56,31 @@ for i in range(len(html)):
                     #f.write(data)
                 #increment to next td
                 j+=1
+
+            #found an indentation mark
+            if 'TEXT-INDENT:' in html[i]:
+                startIndent = html[i].find('TEXT-INDENT:')+13
+                endIndent = html[i].find('pt')
+                #update Indentation of the current indent
+                rowIndent = float(html[i][startIndent:endIndent])
+                print rowIndent
+                try:
+                    #find the next indentation
+                    nx = i+1
+                    while 'TEXT-INDENT:' not in html[nx]:
+                        nx+=1
+
+                    #print html[nx]
+                    startIndent = html[nx].find('TEXT-INDENT:')+13
+                    endIndent = html[nx].find('pt')
+                    #update Indentation of the current indent
+                    nextIndent = float(html[nx][startIndent:endIndent])
+                except IndexError:
+                    nextIndent = rowIndent
+
             #increment to next tr
             i+=1
+
         #TR ended
         if row and row[0]=='\t':
             row = row[1:]
@@ -75,15 +100,30 @@ for i in range(len(html)):
                 data4 = ' '.join(rowArray)
                 row = data4 + data3
                 rowArray = row.split('\t')
-            if validData and len(rowArray) > 2:
-                row+='\n'
-                f.write(row)
+            if validData:
+                #If there is a row less than 2
+                if len(rowArray) < 2:
+                    #indentation fix
+                    if rowIndent < nextIndent:
+                        fix = True
+                        firstRow = True
+                    else:
+                        row += '\n'
+                        fix = False
+                        firstRow = False
+                    prefix = row + ' '
+                #If the row array is not less than 2, but it need to be fixed
+                elif fix:
+                    row = prefix + row + '\n'
+                    if abs(rowIndent - nextIndent) > 1 :
+                        fix = False
+                else:
+                    row += '\n'
+
+                #dont'r write the first row of indentation
+                if firstRow:
+                    firstRow = False
+                    pass
+                else:
+                    f.write(row)
 f.close()
-
-f1 = open('test.txt', 'r')
-f2 = open('test2.txt', 'w')
-
-lines = f1.readlines()
-
-f1.close()
-f2.close()
